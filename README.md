10e17184b50cdfb1c5af6ef6e078d66b4bea160b
aaron a <aaguerrevere@legacy.com>
2021-06-21 11:55:48 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## SQLizer
+
+Script to create `SQL` necessary for [CS-34847](https://legacycom.atlassian.net/browse/CS-34847).
+
+### input(s)
+
+Sharepoint [Janus Communication & analytics Tags](https://legacycom-my.sharepoint.com/:x:/g/personal/kanderson_legacy_com/EVEYhkPexO1IrGP4UJQ1T_wBxT-RArLglPDscFfM5fKymQ?rtime=g6XqOdE02Ug)
---
+++ sqlize.py
@@ -0,0 +1,18 @@
+############------------ IMPORTS ------------############
+import csv
+
+############------------ GLOBAL VARIABLES ------------############
+
+############------------ FUNCTION(S) ------------############
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    pass
+
+
+'''
+USE Legacy
+UPDATE dbo.tblAffiliateSettings
+  SET TrackingHtml = '<INSERT TAG HERE>'
+WHERE AffiliateSiteName = '<INSERT AFFILIATE NAME HERE>
+'''
