65fdc0c0066bd9c49ed1b61d23f622f89184de1d
aaron a <aaguerrevere@legacy.com>
2021-06-21 12:31:05 -0500

    added logic to count how many updates need to happen in this specific ticket

--- sqlize.py
+++ sqlize.py
@@ -3,26 +3,27 @@ import csv
 
 ############------------ FUNCTION(S) ------------############
 def create_sql():
+	'''
+	USE Legacy
+	UPDATE dbo.tblAffiliateSettings
+	SET TrackingHtml = '<INSERT TAG HERE>'
+	WHERE AffiliateSiteName = '<INSERT AFFILIATE NAME HERE>
+	'''
 	with open('JanusCommunicationAnalyticsTags.csv', 'r') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		
-		for i, row in enumerate(csv_reader):
-			if i == 0:
-				print(", ".join(row))
+		in_this_ticket = 0
 
-'''
-Legacy Site Name, Description, Website Name, Corporate Name,
-Email Communication Sent, Analytics Tag, JIRA Ticket, Notes, 
-'''
+		for i, row in enumerate(csv_reader):
+			if row[6] == 'CS-34847':
+				in_this_ticket += 1
+
+		return in_this_ticket
+		
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    create_sql()
+    print(create_sql())
+	# 132
 
 
-'''
-USE Legacy
-UPDATE dbo.tblAffiliateSettings
-  SET TrackingHtml = '<INSERT TAG HERE>'
-WHERE AffiliateSiteName = '<INSERT AFFILIATE NAME HERE>
-'''
