2b61d7ac844a87a7326b9d89da4e2632548cecfc
aaron a <aaguerrevere@legacy.com>
2021-06-21 12:26:44 -0500

    updated README, and added function logic to print headers in csv

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Script to create `SQL` necessary for [CS-34847](https://legacycom.atlassian.net/
 ### input(s)
 
 Sharepoint [Janus Communication & analytics Tags](https://legacycom-my.sharepoint.com/:x:/g/personal/kanderson_legacy_com/EVEYhkPexO1IrGP4UJQ1T_wBxT-RArLglPDscFfM5fKymQ?rtime=g6XqOdE02Ug)
+
+**note** eventually, this will need to get done for **1524** sites
--- sqlize.py
+++ sqlize.py
@@ -1,13 +1,23 @@
 ############------------ IMPORTS ------------############
 import csv
 
-############------------ GLOBAL VARIABLES ------------############
-
 ############------------ FUNCTION(S) ------------############
+def create_sql():
+	with open('JanusCommunicationAnalyticsTags.csv', 'r') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		
+		for i, row in enumerate(csv_reader):
+			if i == 0:
+				print(", ".join(row))
+
+'''
+Legacy Site Name, Description, Website Name, Corporate Name,
+Email Communication Sent, Analytics Tag, JIRA Ticket, Notes, 
+'''
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    create_sql()
 
 
 '''
